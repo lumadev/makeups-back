fc25835f268eac46b3d731f84708784a6a2be576
Luma Macagnan <luma.macagnan@gsurfnet.net>
2025-06-22 21:14:57 -0300

    Initial dependencies and server config

---
+++ index.js
@@ -0,0 +1,30 @@
+// index.js
+import express from 'express'
+import cors from 'cors'
+
+import db from './db.js'
+import studentsRoutes from './routes/students.js'
+import makeupsRoutes from './routes/makeups.js'
+
+const app = express()
+const port = 3000
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+}))
+
+app.use(express.json())
+
+await db.read()
+if (!db.data) {
+  db.data = { alunos: [], reposicoes: [] }
+  await db.write()
+}
+
+// Usa os arquivos de rota
+app.use('/students', studentsRoutes)
+app.use('/makeups', makeupsRoutes)
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
