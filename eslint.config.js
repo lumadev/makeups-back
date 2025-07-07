0996c27548f1ef8cfaafa3958d256d14093a4a61
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-06 22:41:09 -0300

    Added es lint and config file

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginN from "eslint-plugin-n";
+
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: [
+      "node_modules/**",
+      "dist/**",
+      "eslint.config.js"
+    ],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      js,
+      n: pluginN,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...pluginN.configs["recommended"].rules,
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
+]);
