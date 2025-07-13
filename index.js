62d8582e7167798f7cca73c7e4863c04a6522eda
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-13 00:13:00 -0300

    Set up dotenv config and cookie-parser

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 import express from 'express'
 import cors from 'cors'
 import db from './db.js'
-import 'dotenv/config';
+import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.js'
+import cookieParser from 'cookie-parser';
 import studentsRoutes from './routes/students.js'
 import makeupsRoutes from './routes/makeups.js'
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV}`
+});
 
 const app = express()
 const port = 3000
@@ -16,6 +21,7 @@ app.use(cors({
 }))
 
 app.use(express.json())
+app.use(cookieParser());
 
 await db.read()
 if (!db.data) {
