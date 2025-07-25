1f9477a7069e24e5650dac93f23e6af2210c44b2
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-25 20:50:34 -0300

    Remove enter to execute deploy

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ dotenv.config({
 const app = express()
 const port = process.env.PORT || 3000;
 
-
 const corsOrigin = process.env.NODE_ENV === 'development'
   ? 'http://localhost:5173'
   : process.env.FRONTEND_URL;
