b5ea59f48f2a0360ce0a9e5b385842e999d8e62c
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-26 00:58:14 -0300

    Use googleDriveAdapter to return database instance

--- db.js
+++ db.js
@@ -1,15 +1,26 @@
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-
-const adapterDb = new JSONFile('db.json')
-const db = new Low(adapterDb, { alunos: [], reposicoes: [] })
+import { google } from 'googleapis';
+import { Low } from 'lowdb';
+import GoogleDriveAdapter from './src/db/GoogleDriveAdapter.js';
 
 async function initDB() {
-  await db.read()
+  const keyBuffer = Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT_KEY, 'base64');
+  const keyJson = JSON.parse(keyBuffer.toString('utf-8'));
 
-  db.data ||= { alunos: [], reposicoes: [] }
+  const auth = new google.auth.GoogleAuth({
+    credentials: keyJson,
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  });
+  const authClient = await auth.getClient();
 
-  await db.write()
+  // db file from google drive
+  const fileId = '15cf_lJNK569Nq0LGPyHhp-61Og2iVrNtT8MnRwhjyMs';
+
+  const adapter = new GoogleDriveAdapter(authClient, fileId);
+  const rawData = await adapter.read();
+
+  const db = new Low(adapter, rawData);
+
+  await db.read();
 
   return db
 }
