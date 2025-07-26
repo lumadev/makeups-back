76c8c9cadf3987ccfb5f8ab661d84e5f5c03dfee
Luma Macagnan <lumamacagnan@gmail.com>
2025-07-26 01:09:37 -0300

    [Fix] Rename googleDriveAdapter import

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import { google } from 'googleapis';
 import { Low } from 'lowdb';
-import GoogleDriveAdapter from './src/db/GoogleDriveAdapter.js';
+import GoogleDriveAdapter from './src/db/googleDriveAdapter.js';
 
 async function initDB() {
   const keyBuffer = Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT_KEY, 'base64');
