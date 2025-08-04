4c24ca08e446ca27d1a1917a9413ac47d00593f9
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-03 21:27:32 -0300

    organize database: split into makeups, makeups-done, and students

--- src/db/db.js
+++ src/db/db.js
@@ -1,9 +1,37 @@
 import { google } from 'googleapis'
 import { Low } from 'lowdb'
+import { dbTypes } from './dbTypeConsts.js'
 
 import GoogleDriveAdapter from '../../src/db/googleDriveAdapter.js'
 
-async function initDB() {
+/**
+ * Databases in json (Google Drive):
+ * 
+ * makeups
+ * makeups-done
+ * students
+ * 
+ * @param {string} dbType 
+ * @returns google drive's file id
+ */
+function getFileIdByType(dbType) {
+  const idFileMakeups = '15cf_lJNK569Nq0LGPyHhp-61Og2iVrNtT8MnRwhjyMs'
+  const idFileMakeupsDone = '1uufJ-0BVJSBTlJFCGb8eNie32vRlC8buEc11jcDXpGY'
+  const idFileStudents = '1G4Qv_hBakFICCUmLs7wLc2h5i1t_ki2t_-gsqVGzmrs'
+
+  if (dbType === dbTypes.DB_TYPE_MAKEUPS) {
+    return idFileMakeups
+  } else if (dbType === dbTypes.DB_TYPE_MAKEUPS_DONE) {
+    return idFileMakeupsDone
+  } else if (dbType === dbTypes.DB_TYPE_STUDENTS) {
+    return idFileStudents
+  }
+}
+
+/**
+ * Init database in google drive
+ */
+async function initDB(dbType = dbTypes.DB_TYPE_MAKEUPS) {
   const keyBuffer = Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT_KEY, 'base64')
   const keyJson = JSON.parse(keyBuffer.toString('utf-8'))
 
@@ -14,7 +42,7 @@ async function initDB() {
   const authClient = await auth.getClient()
 
   // db file from google drive
-  const fileId = '15cf_lJNK569Nq0LGPyHhp-61Og2iVrNtT8MnRwhjyMs'
+  const fileId = getFileIdByType(dbType)
 
   const adapter = new GoogleDriveAdapter(authClient, fileId)
   const rawData = await adapter.read()
---
+++ src/db/dbTypeConsts.js
@@ -0,0 +1,16 @@
+const DB_TYPE_MAKEUPS = 'makeups'
+const DB_TYPE_MAKEUPS_DONE = 'makeups-done'
+const DB_TYPE_STUDENTS = 'students'
+
+const dbTypes = {
+  DB_TYPE_MAKEUPS,
+  DB_TYPE_MAKEUPS_DONE,
+  DB_TYPE_STUDENTS
+}
+
+export { 
+  dbTypes,
+  DB_TYPE_MAKEUPS,
+  DB_TYPE_MAKEUPS_DONE,
+  DB_TYPE_STUDENTS
+}
