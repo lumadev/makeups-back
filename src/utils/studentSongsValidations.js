3408b3fa163c4fda4d139f0f6ba4a9bfc5faf16a
Luma Macagnan <lumamacagnan@gmail.com>
2025-08-30 18:51:06 -0300

    create student songs validations

---
+++ src/utils/studentSongsValidations.js
@@ -0,0 +1,77 @@
+import { errorFieldsRequired, checkMaxLengths } from './validations.js'
+import { getStudentSongById } from '../services/studentSongsService.js'
+import { getStudentById } from '../services/studentService.js'
+
+const requiredFields = {
+  songName: 'Nome da música',
+  artist: 'Artista',
+  studentId: 'Aluno',
+  isRecital: "Recital",
+  isMusicAudition: "Música da Audição"
+}
+
+const maxLengths = {
+  songName: 150,
+  artist: 100
+}
+
+async function validatePost(req, res, next) {
+  const error = errorFieldsRequired(req.body, requiredFields)
+  if (error) {
+    return res.status(400).json({ error })
+  }
+
+  const lengthError = checkMaxLengths(req.body, maxLengths)
+  if (lengthError) {
+    return res.status(400).json({ error: lengthError })
+  }
+
+  const studentId = Number(req.body.studentId)
+
+  const student = await getStudentById(studentId)
+  if (!student) {
+    return res.status(404).json({ error: 'Aluno não encontrado' })
+  }
+
+  next()
+}
+
+async function validatePut(req, res, next) {
+  const error = errorFieldsRequired(req.body, requiredFields)
+  if (error) {
+    return res.status(400).json({ error })
+  }
+
+  const lengthError = checkMaxLengths(req.body, maxLengths)
+  if (lengthError) {
+    return res.status(400).json({ error: lengthError })
+  }
+
+  const songId = Number(req.params.id)
+  const song = await getStudentSongById(songId)
+  if (!song) {
+    return res.status(404).json({ error: 'Música não encontrada' })
+  }
+
+  const studentId = Number(req.body.studentId)
+
+  const student = await getStudentById(studentId)
+  if (!student) {
+    return res.status(404).json({ error: 'Aluno não encontrado' })
+  }
+
+  next()
+}
+
+async function validateDelete(req, res, next) {
+  const songId = Number(req.params.id)
+
+  const song = await getStudentSongById(songId)
+  if (!song) {
+    return res.status(404).json({ error: 'Música não encontrada' })
+  }
+
+  next()
+}
+
+export { validatePost, validatePut, validateDelete }
