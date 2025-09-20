9516863e97f73073b5ee695de3dd4eee12f7d62f
Luma Macagnan <lumamacagnan@gmail.com>
2025-09-19 23:49:39 -0300

    create updateUser method in service

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -10,4 +10,26 @@ async function getAllUsers() {
   return jokes
 }
 
-export { getAllUsers }
+async function updateUser(userUpdated) {
+  const db = await initDB(DB_TYPE_USERS)
+  await db.read()
+
+  if (!db.data.users) {
+    db.data.users = []
+  }
+
+  const userIndex = db.data.users.findIndex(u => u.id === userUpdated.id)
+  if (userIndex === -1) {
+    throw new Error(`Usuário ${userUpdated.id} não encontrado`)
+  }
+
+  db.data.users[userIndex] = {
+    ...db.data.users[userIndex],
+    ...userUpdated,
+  }
+  await db.write()
+
+  return db.data.users[userIndex]
+}
+
+export { getAllUsers, updateUser }
