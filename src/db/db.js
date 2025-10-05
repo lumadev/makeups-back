42aeafd993376e1c03663f287a84f04d5693b1a5
Luma Macagnan <lumamacagnan@gmail.com>
2025-10-04 22:47:20 -0300

    setup dev database

--- src/db/db.js
+++ src/db/db.js
@@ -4,6 +4,26 @@ import { dbTypes } from './dbTypeConsts.js'
 
 import GoogleDriveAdapter from '../../src/db/googleDriveAdapter.js'
 
+const prodIds = {
+  [dbTypes.DB_TYPE_MAKEUPS]: "15cf_lJNK569Nq0LGPyHhp-61Og2iVrNtT8MnRwhjyMs",
+  [dbTypes.DB_TYPE_MAKEUPS_DONE]: "1uufJ-0BVJSBTlJFCGb8eNie32vRlC8buEc11jcDXpGY",
+  [dbTypes.DB_TYPE_STUDENTS]: "1G4Qv_hBakFICCUmLs7wLc2h5i1t_ki2t_-gsqVGzmrs",
+  [dbTypes.DB_TYPE_STUDENT_SONGS]: "1Rqcqt0PSZiHhjMWmo6hIaUgiJKtnzNPs9rhJf8mTkp8",
+  [dbTypes.DB_TYPE_EVENT_DATES]: "11YGauy7RjGEAXgolvL7xFMqMP18Bii-_DbefZRhEaqQ",
+  [dbTypes.DB_TYPE_JOKES]: "1kNSPddGEZEPzezUwpz6l0Y8Ou-RFK0ma198yHRrTVN8",
+  [dbTypes.DB_TYPE_USERS]: "1lxYz7ge-Ro0P6j1iU_UJHeRF91ih-J41jDcyvwZsnRs",
+}
+
+const devIds = {
+  [dbTypes.DB_TYPE_MAKEUPS]: "1ExdWgzY8WFkKtlnkBandGQA9QxnA4XTbjqT5dEFVHaI",
+  [dbTypes.DB_TYPE_MAKEUPS_DONE]: "1hjTNamg0cNX23hQNwZcDtbCCzJ_Mrjf-nGnFEX6ClPs",
+  [dbTypes.DB_TYPE_STUDENTS]: "1hURtuJHcTk-9ugHwq_lRJbhLpTZPYgP039g8j4FSmyo",
+  [dbTypes.DB_TYPE_STUDENT_SONGS]: "1rc4gyOT6SiQoZlEEdSQ6Q0L1oniIYTAp67cW-wd81F8",
+  [dbTypes.DB_TYPE_EVENT_DATES]: "1WivM7gNFpKK_Hwm6-fWKDeuu3ri76K5HAtWYCHAJdUc",
+  [dbTypes.DB_TYPE_JOKES]: "1toJV3OR8cBELhJPvmHxIvf1s063_TqXXXI_XNUx0vcM",
+  [dbTypes.DB_TYPE_USERS]: "1zH0HuaiZJzId1UYxxuUb6y0wD0-6OQrmNq0peCslasU",
+}
+
 /**
  * Databases in json (Google Drive):
  * 
@@ -19,17 +39,7 @@ import GoogleDriveAdapter from '../../src/db/googleDriveAdapter.js'
  * @returns google drive's file id
  */
 function getFileIdByType(dbType) {
-  // mapping between dbTypes and file IDs
-  const fileIdMap = {
-    [dbTypes.DB_TYPE_MAKEUPS]: '15cf_lJNK569Nq0LGPyHhp-61Og2iVrNtT8MnRwhjyMs',
-    [dbTypes.DB_TYPE_MAKEUPS_DONE]: '1uufJ-0BVJSBTlJFCGb8eNie32vRlC8buEc11jcDXpGY',
-    [dbTypes.DB_TYPE_STUDENTS]: '1G4Qv_hBakFICCUmLs7wLc2h5i1t_ki2t_-gsqVGzmrs',
-    [dbTypes.DB_TYPE_STUDENT_SONGS]: '1Rqcqt0PSZiHhjMWmo6hIaUgiJKtnzNPs9rhJf8mTkp8',
-    [dbTypes.DB_TYPE_EVENT_DATES]: '11YGauy7RjGEAXgolvL7xFMqMP18Bii-_DbefZRhEaqQ',
-    [dbTypes.DB_TYPE_JOKES]: '1kNSPddGEZEPzezUwpz6l0Y8Ou-RFK0ma198yHRrTVN8',
-    [dbTypes.DB_TYPE_USERS]: '1lxYz7ge-Ro0P6j1iU_UJHeRF91ih-J41jDcyvwZsnRs'
-  }
-
+  const fileIdMap = process.env.NODE_ENV === "development" ? devIds : prodIds
   return fileIdMap[dbType] || null
 }
 
