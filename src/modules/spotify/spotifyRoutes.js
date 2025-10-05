ff72399d737baf5c8683144988ef107c2720a3f2
Luma Macagnan <lumamacagnan@gmail.com>
2025-10-04 22:59:47 -0300

    add requireRole to spotify routes

--- src/modules/spotify/spotifyRoutes.js
+++ src/modules/spotify/spotifyRoutes.js
@@ -1,12 +1,13 @@
 import { assignSpotifyToken } from './spotifyAuthService.js'
 import { searchTracks, getTrackDetails } from './spotifyService.js'
-import { verifyToken } from '../../middlewares/authMiddleware.js'
+import { verifyToken, requireRole } from '../../middlewares/authMiddleware.js'
 import { getUserFromToken } from '../../utils/auth.js' 
 
 import express from 'express'
 const router = express.Router()
 
 router.use(verifyToken)
+router.use(requireRole(['full']))
 
 router.post('/credentials', async (req, res) => {
   const user = getUserFromToken(req.cookies?.token)
